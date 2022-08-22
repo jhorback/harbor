7352a30e8ddc5319aa589bf9a9a9da7a178d006a
John Horback <jhorback@gmail.com>
2022-08-21 22:25:11 -0400

    Finished hb-app-bar closes hb-app-bar #31

--- src/layout/hb-app-bar.stories.ts
+++ src/layout/hb-app-bar.stories.ts
@@ -1,6 +1,5 @@
 import { Story, Meta } from '@storybook/web-components';
-import { html, css, LitElement } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { html } from "lit";
 import { iconStyles } from "../styles/iconStyles";
 import "./hb-app-bar";
 
@@ -22,38 +21,22 @@ export default {
 
 
 
-
 export interface AppBarProps {
     showIcons: Boolean
 }
 
-@customElement("hb-app-bar-demo")
-class AppBarDemo extends LitElement {
-
-    @property({type: Boolean})
-    showIcons = true;
-
-    render() {
-        return html`
-            <hb-app-bar>
-                <div slot="buttons" ?hidden=${!this.showIcons}>
-                    <span class="icon-button icon-medium">edit_document</span>
-                    <span class="icon-button icon-medium">add_circle</span>
-                </div>
-            </hb-app-bar>
-        `;
-    }
-
-    static styles = [iconStyles, css`
-        [hidden] {
-            display: none;
-        }
-    `];
-}
 
 
 const AppBarTemplate = ({showIcons}: AppBarProps) => html`
-    <hb-app-bar-demo ?showIcons=${showIcons}></hb-app-bar-demo>
+<hb-app-bar>
+    <div slot="buttons" ?hidden=${!showIcons}>
+        <span class="icon-button icon-medium">edit_document</span>
+        <span class="icon-button icon-medium">add_circle</span>
+    </div>
+</hb-app-bar>
+<style>
+${iconStyles}
+</style>
 `;
 
 const Template: Story<Partial<AppBarProps>> = (args:AppBarProps) => AppBarTemplate(args);
