5d5ffa0e23161c22bbb6ba97c9018781ef8f943e
John Horback <jhorback@gmail.com>
2022-08-22 17:51:02 -0400

    hb-button Closes #32

---
+++ src/common/hb-button.stories.ts
@@ -0,0 +1,53 @@
+import { Story, Meta } from '@storybook/web-components';
+import { html } from 'lit-html';
+import "./hb-button";
+
+
+
+export default {
+    title: 'Common/Button',
+    component: "hb-button",
+    argTypes: {     
+        label: {
+            control: { type: 'text' },
+            description: "The button label"
+        },
+        disabled: {
+            control: { type: "boolean"},
+            description: "Disables the button if true",
+            defaultValue: false,
+            table: {
+                defaultValue: { summary: "false" },
+            }
+        }
+    },
+    parameters: {
+        options: { showPanel: true },
+        actions: {
+            handles: ["click"],
+        }
+    }
+} as Meta;
+
+
+export interface ButtonProps {
+    label: string;
+    disabled: boolean;
+}
+
+
+const ButtonTemplate = ({label, disabled}: ButtonProps) => html`
+    <hb-button
+        ?disabled=${disabled}
+        label=${label}
+    ></hb-avatar>
+`;
+
+const Template: Story<Partial<ButtonProps>> = (args:ButtonProps) => ButtonTemplate(args);
+
+
+export const Button = Template.bind({});
+Button.args = {
+   label: "Button Text",
+   disabled: false
+};
---
+++ src/common/hb-button.ts
@@ -0,0 +1,71 @@
+import { html, css, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { typeStyles } from "../styles/typeStyles";
+
+
+/**
+ * @class Avatar
+ */
+@customElement('hb-button')
+export class Button extends LitElement {
+
+    @property({type: String})
+    label = "";
+
+    @property({type: Boolean})
+    disabled = false;
+
+    render() {
+        return html`
+            <button
+                ?disabled=${this.disabled}
+                class="label-large"
+                @click=${this.handleClick}>
+                ${this.label}
+            </button>
+        `;
+    }
+    
+    handleClick(event:Event) {
+        event.stopImmediatePropagation();
+        event.preventDefault();
+        this.dispatchEvent(new Event("click", {bubbles: true, composed: false}));
+    }
+
+    static styles = [typeStyles, css`
+        :host {
+            display: inline-block;
+        }
+
+        button {
+            height: 40px;
+            padding: 0 1rem;
+            color: var(--md-sys-color-primary);
+            border: 1px solid var(--md-sys-color-outline);
+            border-radius: var(--md-sys-shape-corner-extra-large);
+            background-color: transparent;
+        }
+
+        button:hover {
+            background-color: var(--hb-sys-color-surface-tint2);
+        }
+        button:focus, button:active {
+            outline: none;
+            background-color: var(--hb-sys-color-surface-tint4);
+        }
+        button:active {
+            background-color: var(--hb-sys-color-surface-tint5);
+        }
+        button[disabled], button[disabled]:hover, button[disabled]:active, button[disabled]:focus {
+            color: var(--md-sys-color-on-surface);
+            background-color: transparent;
+            opacity: 0.38;
+        }
+    `]
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'hb-button': Button
+  }
+}
