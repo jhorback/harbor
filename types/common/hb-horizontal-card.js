f43fcce0ff0411c8362ef9da6f95fbaa9bd42c6b
John Horback <jhorback@gmail.com>
2022-10-12 08:25:25 -0400

    Minor style adjustments to the horizontal card

--- types/common/hb-horizontal-card.js
+++ types/common/hb-horizontal-card.js
@@ -22,7 +22,9 @@ let HorizontalCard = class HorizontalCard extends LitElement {
         return html `
             <div class="horizontal-card" @click=${this.handleClick}>
                 <div class="text">
-                    <div class="title-medium readable">${this.text}</div>
+                    <a href=${this.mediaHref}>
+                        <div class="title-medium readable">${this.text}</div>
+                    </a>
                     <div class="body-medium readable">${this.description}</div>
                 </div>
                 <div class="media" ?hidden=${this.mediaUrl === ""}>
@@ -46,9 +48,9 @@ HorizontalCard.styles = [styles.icons, styles.types, css `
             display: block;
         }
         .horizontal-card {
+            overflow: clip;
             display: flex;
             user-select: none;
-            border: 1px solid transparent;
             border-radius:  var(--md-sys-shape-corner-small);
             padding: 0 0 0 10px;
             align-items: center;
@@ -59,8 +61,16 @@ HorizontalCard.styles = [styles.icons, styles.types, css `
         .text {
             flex-grow: 1;
         }
+        a {
+            text-decoration: none;
+
+        }
         .title-medium {
             line-height: 2rem;
+        }
+        .body-medium {
+            max-height: 34px;
+            overflow: clip;
         }
         .readable {
             max-width: 35ch;
