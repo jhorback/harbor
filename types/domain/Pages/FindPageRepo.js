d430340975c6bbf20b0687b10a172878d8e56437
John Horback <jhorback@gmail.com>
2022-11-11 08:07:52 -0500

    Added the add and find page repos

---
+++ types/domain/Pages/FindPageRepo.js
@@ -0,0 +1,20 @@
+import { collection, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import { HbDb } from "../HbDb";
+import { PageModel } from "./PageModel";
+/**
+ * Helper for other repos to ensure the PageModel converter.
+ */
+export class FindPageRepo {
+    async findPage(uid) {
+        const docRef = doc(HbDb.current, "pages", uid).withConverter(PageModel);
+        const docSnap = await getDoc(docRef);
+        return docSnap.exists() ? docSnap.data() : null;
+    }
+    async findPageByPathname(pathname) {
+        const q = query(collection(HbDb.current, "pages"), where("pathname", "==", pathname))
+            .withConverter(PageModel);
+        const snapshot = await getDocs(q);
+        const pages = snapshot.docs.map((page) => page.data());
+        return pages[0];
+    }
+}
