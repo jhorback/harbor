75ff96b8ab057cebb7169d603a5a841e6d1186c5
John Horback <jhorback@gmail.com>
2022-12-22 18:52:09 -0500

    Fixed storybook after node update

--- src/common/hb-link-button.stories.ts
+++ src/common/hb-link-button.stories.ts
@@ -1,6 +1,6 @@
 import { Story, Meta } from '@storybook/web-components';
 import { html } from 'lit-html';
-import { ifDefined } from 'lit-html/directives/if-defined';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
 import "./hb-link-button";
 
 
