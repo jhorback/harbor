5c7d050bd5acefadfce2a3dfd078ba8ef36e81ef
John Horback <jhorback@gmail.com>
2023-01-01 09:01:00 -0500

    Updated the look of the users list

--- src/app/profile/hb-profile-users-tab.ts
+++ src/app/profile/hb-profile-users-tab.ts
@@ -44,7 +44,7 @@ export class ProfileUsersTab extends LitElement {
         }
         .users-container {
             display: grid;
-            grid-template-columns: repeat(2, 374px);
+            grid-template-columns: repeat(3, auto);
             column-gap: 1rem;
             row-gap: 1rem;
         }
